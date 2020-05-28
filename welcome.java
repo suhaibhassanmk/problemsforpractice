e7d074bd8ec2f6171f82cfcf424fd746f292d723
suhaibhassanmk <64411411+suhaibhassanmk@users.noreply.github.com>
2020-05-28 22:21:38 +0530

    Add files via upload

---
+++ welcome.java
@@ -0,0 +1,16 @@
+package myproject;
+
+import java.util.Scanner;
+
+public class welcome 
+{
+	public static void main(String[] args) 
+	{
+		String a;
+		Scanner s = new Scanner(System.in);
+		a=s.nextLine();
+		System.out.println("Welcome "+a);
+
+	}
+
+}
