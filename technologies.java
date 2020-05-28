241ef7be08a0573d4b88ab3a29199fb30ac01ce5
suhaibhassanmk <64411411+suhaibhassanmk@users.noreply.github.com>
2020-05-28 22:16:38 +0530

    Add files via upload

---
+++ technologies.java
@@ -0,0 +1,18 @@
+package myproject;
+import java.util.Scanner;
+
+public class technologies {
+
+	public static void main (String[] args) 
+	{
+		// TODO Auto-generated method stub
+		String a;
+		String b;
+		Scanner s = new Scanner(System.in);
+		a=s.nextLine();
+		b=s.nextLine();
+		System.out.println(a+" Technologies "+b);
+
+	}
+
+}
